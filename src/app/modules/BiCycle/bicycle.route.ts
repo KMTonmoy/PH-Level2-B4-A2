d8a34dbee080fb9af178df4a94e63c444f2f42fc
KMTonmoy <tonmoyahamed2009@gmail.com>
2024-11-21 23:31:09 +0600

    Error handling middleware Fixed

--- src/app/modules/BiCycle/bicycle.route.ts
+++ src/app/modules/BiCycle/bicycle.route.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import { bicycleControllers } from './bicycle.controller';
 
 const router = express.Router();
@@ -7,8 +7,9 @@ router.post('/products', bicycleControllers.createBicycle);
 router.get('/products', bicycleControllers.getAllBicycles);
 router.get('/products/:_id', bicycleControllers.getSingleBicycle);
 
-router.use((err, req, res, next) => {
+router.use((err: Error, req: Request, res: Response, next: NextFunction) => {
     console.error(err);
+    console.error(next);
     res.status(500).json({
         success: false,
         message: 'Something went wrong',
